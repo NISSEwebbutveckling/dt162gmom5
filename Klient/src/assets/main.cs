0a702ddbe6d86b9a74387f15649fb1b2eb4b35ad
NISSEwebbutveckling <98022157+NISSEwebbutveckling@users.noreply.github.com>
2023-01-14 16:53:17 +0100

    main.cs

---
+++ Klient/src/assets/main.cs
@@ -0,0 +1,269 @@
+/*
+DT162G, moment5 - projekt
+//Nils, 
+//nibo1005@student.miun.se
+Lägger viss css i main.css, istället för lägga direkt på komponent.
+*/
+
+
+/*Body-elemtet, mfl. Anger olika property och values.*/
+
+body {
+    margin: 0;
+    padding: 0;
+    font-family: Spartan, 'Courier New', Courier, monospace;
+    font-size: 1.2em;
+    line-height: 1.8em;
+    color: rgb(0, 15, 15);
+    background-color: #E6E18F;
+}
+
+nav {
+    background-color: #FFFAE2;
+    font-size: 1.5em;
+}
+
+li .nav-link {
+    color: black;
+}
+
+header {
+    margin-top: 0em;
+    padding: 0em;
+    background-color: #92977E;
+    margin-bottom: 2%;
+}
+
+.container {
+    display: flex;
+    justify-content: space-between;
+    max-width: 1000px;
+    width: 100%;
+    margin: 0 auto;
+    margin-top: -2%;
+}
+
+.card {
+    padding: 1%;
+    margin-top: 1%;
+    margin-bottom: 1%;
+    background-color: #FFFAE2;
+    font-size: 1.1em;
+    border-radius: 1em 0em 0 0em;
+    box-shadow: 0px 4px 18px 5px rgba(0, 0, 0, 0.20);
+}
+
+footer {
+    margin-top: 2%;
+    margin-bottom: -4%;
+    background-color: #EADDA6;
+    border-top: grey solid 0.2em;
+}
+
+footer li {
+    text-align: center;
+    list-style: none;
+}
+
+.toplink {
+    display: none;
+}
+
+
+/*Rubriker*/
+
+header h1 {
+    font-family: 'Spartan', Courier, monospace;
+    float: left;
+    margin-top: 6%;
+    margin-left: 3%;
+    font-size: 2.6em;
+    margin-right: 0;
+    transform: rotate(0deg);
+    color: whitesmoke;
+}
+
+.cardcard .card form h4 {
+    text-align: center;
+}
+
+.card h2 {
+    margin-top: 2%;
+}
+
+.card form,
+.card .UppdateraH2 {
+    text-align: center;
+}
+
+
+/*Tabell*/
+
+thead,
+table.table {
+    background-color: #E6E18F;
+}
+
+thead:hover,
+table.table:hover {
+    background-color: #E6E18F;
+}
+
+td {
+    text-align: left;
+}
+
+
+/**/
+
+.card h2,
+.card h1,
+.card p,
+.card li,
+.card h3,
+.card video,
+.card figcaption {
+    text-align: left;
+    margin-left: 2%;
+}
+
+
+/**/
+
+.nav-link {
+    color: rgb(1, 18, 92);
+    font-weight: bolder;
+    padding: 1em;
+    margin-right: 1em;
+}
+
+.nav-link:hover {
+    color: blueviolet;
+}
+
+
+/*Bild*/
+
+header img {
+    margin-bottom: 0%;
+    margin-left: 30%;
+    margin-right: 1%;
+    width: 22%;
+    margin-top: 0%;
+    transform: rotate(-45deg);
+}
+
+.card .profilimg {
+    width: 20%;
+    float: right;
+    margin-right: 10%;
+    border-radius: 1em;
+}
+
+.card img {
+    width: 50%;
+    margin-left: 25%;
+}
+
+.card .gif {
+    margin-left: 2%;
+    width: 20%;
+    transform: rotate(0deg);
+}
+
+.card .elgitarr {
+    margin-left: 25%;
+    width: 40%;
+    transform: rotate(40deg);
+}
+
+header #mobilheader {
+    display: none;
+}
+
+
+/*Länk*/
+
+.toplink2 {
+    display: block;
+    text-align: center;
+    margin-top: 3%;
+    margin-bottom: 3%;
+    margin-right: 25%;
+}
+
+.toplink2 a {
+    font-size: 1.2em;
+    margin-left: 30%;
+    text-decoration: none;
+    color: black;
+}
+
+
+/* Responsiv design för max 800px -------------------------------*/
+
+@media only screen and (max-width: 800px) {
+    .toplink {
+        display: none;
+    }
+    .toplink a {
+        font-size: 1.2em;
+        margin-left: 40%;
+        text-decoration: none;
+        color: black;
+    }
+    header h1 {
+        display: block;
+        font-size: 1.1em;
+    }
+    header img {
+        margin-bottom: 0%;
+        width: 30%;
+        margin-top: 1%;
+        margin-right: 7%;
+        transform: rotate(0deg);
+    }
+}
+
+
+/* Responsiv design för max 500px -------------------------------*/
+
+@media only screen and (max-width: 500px) {
+    .toplink {
+        display: block;
+        text-align: center;
+        margin-right: 25%;
+    }
+    .toplink a {
+        font-size: 1.2em;
+        margin-left: 30%;
+        text-decoration: none;
+        color: black;
+    }
+    img .profilimg,
+    header h1,
+    .toplink2 {
+        display: none;
+    }
+    header #mobilheader {
+        display: block;
+        font-size: 1.7em;
+        margin-right: 12%;
+        margin-bottom: 4%;
+        margin-top: 2%;
+    }
+    header img {
+        margin-bottom: 0%;
+        margin-right: 10%;
+        width: 40%;
+        margin-top: 1%;
+        transform: rotate(0deg);
+    }
+    li {
+        text-align: center;
+        text-decoration: none;
+    }
+    .card img {
+        display: none;
+    }
+}
