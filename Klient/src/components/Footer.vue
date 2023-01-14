0467d454e2bc5c641558ebfade239d9a075c809c
NISSEwebbutveckling <98022157+NISSEwebbutveckling@users.noreply.github.com>
2023-01-14 17:11:06 +0100

    Footer.vue

---
+++ Klient/src/components/Footer.vue
@@ -0,0 +1,29 @@
+<!--Komponent Footer, single file component.-->
+<!--Skapar komponent som blir header.-->
+<template>
+<!--DT162G, moment5 - projekt
+    //Nils, 
+    //nibo1005@student.miun.se-->
+  <!--Sidfoten, lika på alla sidor.-->
+    <footer><br><br>
+        <div class="card-body">
+
+            <br>
+            <li class="nav-item">
+                <a href="/musics">>Användarsidan
+                    <span class="glyphicon glyphicon-user"></span>
+                </a>
+            </li>
+            <h6 class="card-title" style="text-align: center;">
+                <br>
+                <br> DT162G, Javascriptbaserad webbutveckling
+                <br>
+                <br> Projektuppgift<br> nibo1005@student.miun.se
+                <br>
+                <br> (C)
+                <br>
+                <br><br><br><br><br><br>
+            </h6>
+        </div>
+    </footer>
+    </template>
